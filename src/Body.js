40f7eeeb1d566bdb88ec659bb3963845c36ce322
darrk-heart <prospaul999@gmail.com>
2024-05-20 10:19:50 +0000

    working

--- src/Body.js
+++ src/Body.js
@@ -3,15 +3,27 @@ import React from "react";
 function Body() {
   return (
     <div>
-      <div>
-        <div>calc</div>
-        <div>
-          <span>THEME</span>
-          <div></div>
+      <div
+        style={{
+          width: "33%",
+          height: "70%",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          position: "absolute",
+          backgroundColor: "blue",
+        }}
+      >
+        <div style={{ backgroundColor: "red" }}>
+          <div>calc</div>
+          <div>
+            <span>THEME</span>
+            <div></div>
+          </div>
         </div>
+        <div></div>
+        <div></div>
       </div>
-      <div></div>
-      <div></div>
     </div>
   );
 }
