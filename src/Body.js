fe609cb3857d42693d4af3c7aa916e90a7adcda6
darrk-heart <prospaul999@gmail.com>
2024-05-23 09:25:45 +0000

    formatted code with es6 syntax

--- src/Body.js
+++ src/Body.js
@@ -3,68 +3,18 @@ import ToggleTheme from "./ToggleTheme";
 
 function Body() {
   return (
-    <div
-      style={{
-        backgroundColor: "#3b4664",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          width: "33%",
-          height: "70%",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          position: "absolute",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            color: "white",
-          }}
-        >
-          <div style={{ fontSize: "1.2rem", fontWeight: "700" }}>calc</div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              width: "28%",
-              justifyContent: "space-between",
-            }}
-          >
-            <span style={{ fontSize: "0.6rem", fontWeight: "600" }}>THEME</span>
+    <div className="main-body">
+      <div className="body-frame">
+        <div className="frame-one">
+          <div className="calc">calc</div>
+          <div className="frame-one-content">
+            <span className="theme">THEME</span>
             <ToggleTheme />
           </div>
         </div>
-        <div
-          style={{
-            height: "17%",
-            margin: "25px 0 20px 0",
-            borderRadius: "8px",
-            backgroundColor: "#181f32",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: "#252d44",
-            height: "60%",
-            borderRadius: "8px",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "30px 30px",
-            }}
-          >
+        <div className="frame-two"></div>
+        <div className="frame-three">
+          <div>
             <div className="numbers">7</div>
             <div className="numbers">8</div>
             <div className="numbers">9</div>
